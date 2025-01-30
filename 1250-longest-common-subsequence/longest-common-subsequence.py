ec54c3a61e2b103acc6914e6f7a1dc1d1f2578c4
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-01-30 20:28:23 +0530

    Time: 495 ms (49.93%) | Memory: 43.6 MB (27.32%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -13,4 +13,14 @@ class Solution:
             return c
         m=len(str1)
         n=len(str2)
-        return lcs(0,0,m,n)
+        #return lcs(0,0,m,n)
+
+
+        dp=[[0 for i in range(n+1)] for j in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if str1[i-1]==str2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[m][n]
