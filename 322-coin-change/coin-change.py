9242f5f89fdf7a69d80e04aba4395d0891b81f62
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-01-31 15:10:51 +0530

    Time: 790 ms (46.61%) | Memory: 18.3 MB (51.67%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,18 +1,31 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        val=[0]
-        @lru_cache(None)
-        def recur(amt):
-            if amt==0:
-                return 0
-            val=math.inf
-            for i in coins:
-                if amt-i>=0:
-                    val=min(val,1+recur(amt-i))
-            return val
+        # val=[0]
+        # @lru_cache(None)
+        # def recur(amt):
+        #     if amt==0:
+        #         return 0
+        #     val=math.inf
+        #     for i in coins:
+        #         if amt-i>=0:
+        #             val=min(val,1+recur(amt-i))
+        #     return val
         
-        val=recur(amount)
-        print(val)
-        if val==math.inf:
+        # val=recur(amount)
+        # print(val)
+        # if val==math.inf:
+        #     return -1
+        # return val
+
+        noOfCoins=len(coins)
+        dp=[math.inf for i in range(amount+1)] 
+        dp[0]=0
+        for j in coins:
+            for i in range(1,amount+1):
+                if i>=j:
+                    dp[i]=min(dp[i],dp[i-j]+1)
+        if dp[amount]==math.inf:
             return -1
-        return val
+        return dp[amount]
+
+                
