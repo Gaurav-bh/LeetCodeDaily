ea71140c35b06a598469f8ff00ffad917e6fa9f6
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-02-01 12:30:06 +0530

    Time: 274 ms (28.76%) | Memory: 27.4 MB (29.70%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,18 +1,34 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @lru_cache(None)
-        def recur(i,state,n):
-            if i==n:
-                return 0
-            dontg=recur(i+1,state,n)
-            something=0
-            if state==0:
-                something=recur(i+1,1,n)-prices[i]
-            else:
-                something=prices[i]+recur(i+1,0,n)-fee
-            return max(dontg,something)
+        # @lru_cache(None)
+        # def recur(i,state,n):
+        #     if i==n:
+        #         return 0
+        #     dontg=recur(i+1,state,n)
+        #     something=0
+        #     if state==0:
+        #         something=recur(i+1,1,n)-prices[i]
+        #     else:
+        #         something=prices[i]+recur(i+1,0,n)-fee
+        #     return max(dontg,something)
         n=len(prices)
-        return recur(0,0,n)
+        # return recur(0,0,n)
+
+        maxProfits=[[0,0] for i in range(n+1)]
+        for i in range(n-1,-1,-1):
+            for state in range(2):
+                dontg=maxProfits[i+1][state]
+                if state==0:
+                    something=maxProfits[i+1][1]-prices[i]
+                else:
+                    something=prices[i]+maxProfits[i+1][0]-fee
+                maxProfits[i][state]=max(dontg,something)
+        return maxProfits[0][0]
+
+
+
+
+
 
 
         
