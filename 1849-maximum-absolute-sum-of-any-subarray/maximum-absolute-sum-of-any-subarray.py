2ee09590b11d13319721fc2ba00a8a077da3c5a8
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-02-26 07:20:19 +0530

    Time: 106 ms (16.83%) | Memory: 28.4 MB (91.71%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,18 +1,30 @@
 class Solution:
-    def maxAbsoluteSum(self, nums: List[int]) -> int:
-        currSum=0
-        maxSum=0
-        for i in nums:
-            currSum=currSum+i
-            maxSum=max(currSum,maxSum)
-            if currSum<0:
-                currSum=0
-        currSum=0
-        for i in nums:
-            currSum=currSum+i
-            maxSum=max(abs(currSum),maxSum)
-            if currSum>0:
-                currSum=0
-        return maxSum
-            
-        
+    def maxAbsoluteSum(self, nums):
+        min_prefix_sum = float("inf")
+        max_prefix_sum = float("-inf")
+        prefix_sum = 0
+        max_abs_sum = 0
+
+        for num in nums:
+            # Prefix sum from index 0 to i
+            prefix_sum += num
+
+            # Minimum & Maximum prefix sum we have seen so far
+            min_prefix_sum = min(min_prefix_sum, prefix_sum)
+            max_prefix_sum = max(max_prefix_sum, prefix_sum)
+
+            if prefix_sum >= 0:
+                # If the prefix_sum is positive, we will get the difference
+                # between prefix_sum & min_prefix_sum
+                max_abs_sum = max(
+                    max_abs_sum, max(prefix_sum, prefix_sum - min_prefix_sum)
+                )
+            elif prefix_sum <= 0:
+                # If the prefix_sum is negative, we will get the absolute difference
+                # between prefix_sum & max_prefix_sum
+                max_abs_sum = max(
+                    max_abs_sum,
+                    max(abs(prefix_sum), abs(prefix_sum - max_prefix_sum)),
+                )
+
+        return max_abs_sum
