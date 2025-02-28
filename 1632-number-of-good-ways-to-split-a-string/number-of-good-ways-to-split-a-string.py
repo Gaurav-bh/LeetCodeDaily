22e82c67539e3914368cb7c72c151330aed6fd81
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-02-28 18:37:45 +0530

    Time: 54 ms (78.43%) | Memory: 19.3 MB (16.33%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.py
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.py
@@ -1,12 +1,14 @@
 class Solution:
     def numSplits(self, s: str) -> int:
+        
         n=len(s)
-        prefix=[0 for i in range(n)]
-        suffix=[0 for i in range(n)]
+        prefix=[0]*n
+        suffix=[0]*n
         charSet=set()
         for i in range(n):
             charSet.add(s[i])
             prefix[i]=len(charSet)
+        
         charSet=set()
         for i in range(n-1,-1,-1):
             charSet.add(s[i])
