033b8e22e876514fd1b4dfb0532770231e027457
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-03-05 17:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.93%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
@@ -1,26 +1,18 @@
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        def helper(lower = float('-inf'), upper = float('inf')):
-            nonlocal idx
-            # If all elements from preorder are used
-            # Then the tree is constructed
-            if idx == n:
-                return None
-            
-            val = preorder[idx]
-            # If the current element 
-            # couldn't be placed here to meet BST requirements
-            if val < lower or val > upper:
-                return None
-            
-            # place the current element
-            # and recursively construct subtrees
-            idx += 1
-            root = TreeNode(val)
-            root.left = helper(lower, val)
-            root.right = helper(val, upper)
-            return root
-        
-        idx = 0
-        n = len(preorder)
-        return helper()
+        stack=[]
+        start=TreeNode(preorder[0])
+        n=len(preorder)
+        stack.append(start)
+        #print(stack)
+        for i in range(1,n):
+            node,child=stack[-1],TreeNode(preorder[i])
+            while stack and stack[-1].val<child.val:
+                node=stack.pop()
+            if node.val<child.val:
+                node.right=child
+            else:
+                node.left=child
+            stack.append(child)
+            #print(stack)
+        return start
