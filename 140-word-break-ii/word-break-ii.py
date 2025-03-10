808af6fe4dd354f67435add275250e44a998916a
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-03-10 13:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.89%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict=set(wordDict)
+        n=len(s)
+        dp={}
+        def recursion(ind,st,curr,ans):
+            if len(st)==0:
+                s=""
+                l=len(curr)
+                for i in range(l):
+                    if i==l-1:
+                        s+=curr[i]
+                    else:
+                        s+=curr[i]+" "
+                ans.append(s)
+                return True
+            
+            #print("ind",ind)
+            for i in range(len(st)):
+                #print('i:-',i)
+                #print(st[:i+1])
+                if st[:i+1] in wordDict:
+                    #print("s",st)
+                    #print(st[:i+1])
+                    #print(st[i+1:])
+                    curr.append(st[:i+1])
+                    recursion(i+1,st[i+1:],curr,ans)
+                    curr.pop()
+           
+            return False
+        curr=[]
+        ans=[]
+
+        recursion(0,s,curr,ans)
+        print(ans)
+        return ans
+        
