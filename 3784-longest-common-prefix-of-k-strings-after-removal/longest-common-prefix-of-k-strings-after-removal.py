fac27fff25d66c35f5ca01ceb2e81d0aa85fc10e
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-03-15 22:13:02 +0530

    Time: 1413 ms (0.00%) | Memory: 37.2 MB (0.00%) - LeetSync

--- 3784-longest-common-prefix-of-k-strings-after-removal/longest-common-prefix-of-k-strings-after-removal.py
+++ 3784-longest-common-prefix-of-k-strings-after-removal/longest-common-prefix-of-k-strings-after-removal.py
@@ -1,50 +1,54 @@
-class TrieNode:
+class Trie:
     def __init__(self):
-        self.children = [None] * 26
+        self.children = [None for i in range(26)]
         self.count = 0
-        self.bestPrefixLength = -1
-        self.isEndOfWord = False
+        
+    def addWord(self, word, first_max, second_max, k):
+        current = self
+        flag = True
+        
+        for i in range(len(word)):
+            index = ord(word[i]) - ord('a')
+            if not current.children[index]:
+                current.children[index] = Trie()
+            current = current.children[index] 
+            current.count += 1
+            if current.count >= k:
+                print("Yes")
+                if len(first_max[0]) <= i + 1:
+                    print("ha")
+                    if flag: 
+                        second_max[0] = first_max[0]
+                    
+                    first_max[0] = word[:i + 1]
+                    flag = False
+                elif  flag and len(second_max[0]) <= i + 1:
+                    second_max[0] = word[: i + 1]
+                    
+                    
 
 class Solution:
     def longestCommonPrefix(self, words: List[str], k: int) -> List[int]:
-        totalWords = len(words)
-        result = [0] * totalWords
-        if totalWords - 1 < k:
-            return result
-        self.root = TrieNode()
-        for word in words:
-            #insert the word with increasing the count by 1 (prefix count)
-            self.updateTrie(word, 1, k)
-        for i in range(totalWords):
-            #temp deletion of the word with count decreased by 1
-            self.updateTrie(words[i], -1, k)
-            result[i] = self.root.bestPrefixLength
-            #re-insertion of the word
-            self.updateTrie(words[i], 1, k)
-        return result
-    
-    def updateTrie(self, word: str, delta: int, k: int) -> None:
-        wordLength = len(word)
-        #used to store the path used during trie travesal to update the count and use the count
-        nodePath = [None] * (wordLength + 1)
-        depths = [0] * (wordLength + 1)
-        nodePath[0] = self.root
-        depths[0] = 0
-        #trie insertion
-        for i in range(wordLength):
-            letterIndex = ord(word[i]) - ord('a')
-            if nodePath[i].children[letterIndex] is None:
-                nodePath[i].children[letterIndex] = TrieNode()
-            nodePath[i + 1] = nodePath[i].children[letterIndex]
-            depths[i + 1] = depths[i] + 1
-        #increase the count of each prefix 
-        for node in nodePath:
-            node.count += delta
-        #find the best prefix
-        for i in range(wordLength, -1, -1):
-            currentNode = nodePath[i]
-            candidate = depths[i] if currentNode.count >= k else -1
-            for childNode in currentNode.children:
-                if childNode is not None:
-                    candidate = max(candidate, childNode.bestPrefixLength)
-            currentNode.bestPrefixLength = candidate
+        first_max = [""]
+        second_max = [""]
+        
+        myTrie = Trie()
+        
+        for i in range(len(words)):
+            myTrie.addWord(words[i], first_max, second_max, k)
+            print(first_max)
+            print(second_max)
+            
+        
+        answer = [0 for i in range(len(words))]
+        for i in range(len(words)):
+            if words[i].find(first_max[0]) == 0:
+                answer[i] = len(second_max[0])
+            else:
+                answer[i] = len(first_max[0])
+            
+        
+        return  answer
+            
+            
+            
