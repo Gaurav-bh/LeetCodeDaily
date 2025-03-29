a512434550d6a2192769180f707dbae28d11df4e
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-03-29 17:01:56 +0530

    Time: 24 ms (49.43%) | Memory: 18.7 MB (78.66%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -2,19 +2,18 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         m=len(matrix)
         n=len(matrix[0])
-        paths=[[0 for i in range(n)] for j in range(m) ]
-        paths[0]=matrix[0]
+        
         if n==1:
             return matrix[0][0]
 
         for i in range(1,m):
             for j in range(n):
                 if j==0:
-                    paths[i][j]=matrix[i][j]+min(paths[i-1][j],paths[i-1][j+1])
+                    matrix[i][j]=matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j+1])
                 elif j==n-1:
-                    paths[i][j]=matrix[i][j]+min(paths[i-1][j],paths[i-1][j-1])
+                    matrix[i][j]=matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j-1])
                 else:
-                    paths[i][j]=matrix[i][j]+min(min(paths[i-1][j],paths[i-1][j-1]),paths[i-1][j+1])
-        print(paths)
-        return min(paths[-1])
+                    matrix[i][j]=matrix[i][j]+min(min(matrix[i-1][j],matrix[i-1][j-1]),matrix[i-1][j+1])
+        
+        return min(matrix[-1])
 
