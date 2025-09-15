cb080505ae0758620813a25e553eca8aacd9a94e
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-09-15 22:39:03 +0530

    Time: 7 ms (82.55%) | Memory: 17.9 MB (40.06%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,23 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        n = len(candidates)
-        def targetSumComb(sum, curr, ans, ind):
-            print(sum)
-            print(curr)
-            if sum == 0:
-                
+        n=len(candidates)
+        def recur(s,ans,curr,ind):
+            if s==0:
                 ans.append(curr.copy())
                 return 
-            for i in range(ind, n):
-                candi = candidates[i]
-                if candi <= sum:
-                    curr.append(candi)
-                    targetSumComb(sum-candi, curr, ans, i)
-                    curr.pop()
-        ans = []
-        curr = []
-        candidates.sort()
-        targetSumComb(target, curr, ans, 0)
+            if s<0:
+                return 
+            for i in range(ind,n):
+                recur(s-candidates[i],ans,curr+[candidates[i]],i)
+        ans=[]
+        recur(target,ans,[],0)
         return ans
-        
         
