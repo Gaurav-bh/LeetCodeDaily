1089872f5602958f7acdcbd0cf68411070469f06
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-09-15 22:14:35 +0530

    Time: 11 ms (13.61%) | Memory: 18.3 MB (94.04%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,17 +1,24 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        n=len(s)
-        def helper(curr,pos,ans):
-            if pos==n:
-                ans.append(curr)
+        n = len(s)
+        def noOfStr(ind, ans, s):
+           # print("yes1",s)
+            if ind == n:
+                #print("added")
+                ans.append(s)
                 return 
-            if s[pos] in ("1","2","3","4","5","6","7","8","9","0"):
-                helper(curr+s[pos],pos+1,ans)
+            char = s[ind]
+            if char.isalpha():
+                #print("yes")
+                noOfStr(ind+1, ans, s[:ind] + char.lower() + s[ind+1:])
+                noOfStr(ind+1, ans, s[:ind] + char.upper() + s[ind+1:])
             else:
-                helper(curr+s[pos].lower(),pos+1,ans)
-                helper(curr+s[pos].upper(),pos+1,ans)
-        ans=[]
-        helper("",0,ans)
+                noOfStr(ind+1, ans, s)
+
+        ans = []
+        noOfStr(0, ans, s)
         return ans
+
+
          
         
