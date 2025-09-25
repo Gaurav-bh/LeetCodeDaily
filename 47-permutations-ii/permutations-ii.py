d532d52e160f62b9aa648a83c3c820e34a5d7e18
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-09-25 15:49:57 +0530

    Time: 3 ms (94.23%) | Memory: 18.2 MB (38.14%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,11 +1,30 @@
+from typing import List
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        self.ans = set()
-        def get_perms(nums, lst):
-            if not nums:
-                self.ans.add(tuple(lst.copy()))
+        nums.sort()  # sort so duplicates are adjacent
+        ans = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                ans.append(path[:])
                 return
+            
             for i in range(len(nums)):
-                get_perms(nums[:i] + nums[i+1:], lst + [nums[i]])
-        get_perms(nums, [])
-        return list(self.ans)
+                # skip already used numbers
+                if used[i]:
+                    continue
+                
+                # skip duplicates (only if the previous identical number wasn't used)
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return ans
