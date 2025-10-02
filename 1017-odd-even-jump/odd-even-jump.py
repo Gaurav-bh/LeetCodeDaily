c6987b78b7769b1bcea347f1c31ecd4a2fdde111
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-10-02 19:35:58 +0530

    Time: 63 ms (91.35%) | Memory: 21.2 MB (82.97%) - LeetSync

--- 1017-odd-even-jump/odd-even-jump.py
+++ 1017-odd-even-jump/odd-even-jump.py
@@ -1,36 +1,29 @@
-from sortedcontainers import SortedDict
 class Solution(object):
-    
-
-    def oddEvenJumps(self,A):
+    def oddEvenJumps(self, A):
         N = len(A)
-        if N <= 1:
-            return N
-        
+
+        def make(B):
+            ans = [None] * N
+            stack = []  # invariant: stack is decreasing
+            for i in B:
+                while stack and i > stack[-1]:
+                    ans[stack.pop()] = i
+                stack.append(i)
+            return ans
+
+        B = sorted(range(N), key = lambda i: A[i])
+        oddnext = make(B)
+        B.sort(key = lambda i: -A[i])
+        evennext = make(B)
+
         odd = [False] * N
         even = [False] * N
-        odd[-1] = even[-1] = True
-        
-        vals = SortedDict()
-        vals[A[-1]] = N - 1
-        
-        for i in range(N - 2, -1, -1):
-            v = A[i]
-            
-            if v in vals:
-                odd[i] = even[vals[v]]
-                even[i] = odd[vals[v]]
-            else:
-                lower = vals.iloc[vals.bisect_left(v) - 1] if vals.bisect_left(v) > 0 else None
-                higher = vals.iloc[vals.bisect_right(v)] if vals.bisect_right(v) < len(vals) else None
-                
-                if lower is not None:
-                    even[i] = odd[vals[lower]]
-                if higher is not None:
-                    odd[i] = even[vals[higher]]
-            
-            vals[v] = i
-        
+        odd[N-1] = even[N-1] = True
+
+        for i in range(N-2, -1, -1):
+            if oddnext[i] is not None:
+                odd[i] = even[oddnext[i]]
+            if evennext[i] is not None:
+                even[i] = odd[evennext[i]]
+
         return sum(odd)
-
-
