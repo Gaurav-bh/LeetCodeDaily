2dd1100ab8c1f2fa6df287248836b4135e3717fe
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-10-07 21:35:03 +0530

    Time: 196 ms (6.68%) | Memory: 18 MB (31.75%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.py
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.py
@@ -1,13 +1,17 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
-        stack = [float('inf')]
-        res = 0
-        for num in arr:
-            while stack and stack[-1] <= num:
-                cur = stack.pop()
-                if stack:
-                    res += cur * min(stack[-1], num)
-            stack.append(num)
-        while len(stack) > 2:
-            res += stack.pop() * stack[-1]
+        return self.helper(arr, 0, len(arr) - 1, {})
+        
+    def helper(self, arr, l, r, cache):
+        if (l, r) in cache:
+            return cache[(l, r)]
+        if l >= r:
+            return 0
+        
+        res = float('inf')
+        for i in range(l, r):
+            rootVal = max(arr[l:i+1]) * max(arr[i+1:r+1])
+            res = min(res, rootVal + self.helper(arr, l, i, cache) + self.helper(arr, i + 1, r, cache))
+        
+        cache[(l, r)] = res
         return res
