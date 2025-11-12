d28f0be2726295b99e1f6a6e8ad3dd2d63c9c40f
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-11-12 08:49:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.65%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -1,36 +1,26 @@
-import math
-from functools import reduce
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        
-        def gcd_of_array(arr):
-            return reduce(math.gcd, arr)
-
         n = len(nums)
-        ans = 999999999
-        flag = False
-        oneFlag = False
+        num1 = 0
+        g = 0
+
+        for x in nums:
+            if x == 1:
+                num1 += 1
+            g = gcd(g, x)
+
+        if num1 > 0:
+            return n - num1
+        if g > 1:
+            return -1
+
+        min_len = n
         for i in range(n):
-            arr = []
-            for j in range(i,n):
-                arr.append(nums[j])
-                if nums[j] == 1:
-                    oneFlag = True
-                print(arr)
-                if gcd_of_array(arr)==1:
-                    flag = True
-                    ans = min(ans,(j-i))
+            g = 0
+            for j in range(i, n):
+                g = gcd(g, nums[j])
+                if g == 1:
+                    min_len = min(min_len, j - i + 1)
+                    break
 
-        if oneFlag:
-            count = 0
-            for i in range(n):
-                if nums[i]==1:
-                    count +=1
-            return n-count
-
-        if flag:
-            return ans+n-1
-        return -1
-
-
-        
+        return min_len + n - 2
