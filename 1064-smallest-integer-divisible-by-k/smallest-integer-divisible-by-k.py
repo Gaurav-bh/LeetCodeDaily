af421d677d40903f672d5f6e556d8d9242c9a357
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-11-25 19:13:51 +0530

    Time: 11 ms (53.34%) | Memory: 22.7 MB (5.01%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        n = 1
+        count = 1
+        hash = {}
+        while True:
+            mod = n%k
+            if mod==0:
+                return count
+            if mod in hash:
+                return -1 
+            hash[mod] = True
+            n = mod*10 +1
+            count += 1
+        return -1
+        
