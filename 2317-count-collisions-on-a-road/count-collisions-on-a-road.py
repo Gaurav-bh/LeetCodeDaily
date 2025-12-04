54c7c87019e7ef866a0a0fca7ab45c7346367322
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-12-04 20:46:53 +0530

    Time: 83 ms (68.77%) | Memory: 18.4 MB (39.78%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
@@ -2,19 +2,19 @@ class Solution:
     def countCollisions(self, directions: str) -> int:
         res = 0
         flag = -1
-
-        for c in directions:
-            if c == "L":
-                if flag >= 0:
-                    res += flag + 1
+        for i in directions:
+            if i == "L":
+                if flag>=0:
+                    res += flag+1
                     flag = 0
-            elif c == "S":
-                if flag > 0:
+                    
+            elif i == "S":
+                if flag>0:
                     res += flag
                 flag = 0
             else:
-                if flag >= 0:
-                    flag += 1
-                else:
+                if flag<=0:
                     flag = 1
+                else:
+                    flag +=1
         return res
