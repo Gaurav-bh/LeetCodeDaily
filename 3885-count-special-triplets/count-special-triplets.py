a93446805c079caa289b94794e0c0e3ef1047fe3
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-12-09 21:56:59 +0530

    Time: 797 ms (42.79%) | Memory: 41.5 MB (69.82%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.py
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -1,18 +1,9 @@
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        MOD = 10**9 + 7
-        num_cnt = {}
-        num_partial_cnt = {}
-
-        for v in nums:
-            num_cnt[v] = num_cnt.get(v, 0) + 1
-
-        ans = 0
-        for v in nums:
-            target = v * 2
-            l_cnt = num_partial_cnt.get(target, 0)
-            num_partial_cnt[v] = num_partial_cnt.get(v, 0) + 1
-            r_cnt = num_cnt.get(target, 0) - num_partial_cnt.get(target, 0)
-            ans = (ans + l_cnt * r_cnt) % MOD
-
-        return ans
+        freqPrev, freqNext = Counter(), Counter(nums)
+        count = 0
+        for num in nums:
+            freqNext[num] -= 1
+            count += freqPrev[num*2] * freqNext[num*2]
+            freqPrev[num] += 1
+        return count % 1000000007
