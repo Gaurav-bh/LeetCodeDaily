19cea5f50fa203481adae0a5f93e81bbf00fda67
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2025-12-20 19:04:53 +0530

    Time: 508 ms (5.03%) | Memory: 19.7 MB (7.33%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        n = len(strs[0])
+        k = [[strs[0][i]] for i in range(n)]
+        ans = set()
+        for j in range(1,len(strs)):
+            s = strs[j]
+            for i in range(n):
+                curr = s[i]
+                col = k[i][-1]
+                print(curr,col)
+                if col>curr:
+                    
+                    ans.add(i)
+                k[i].append(curr)
+        return len(ans)
+            
+        
