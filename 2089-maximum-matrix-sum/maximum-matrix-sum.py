a3f3bf270f1dcacd0696b08d23d0adf5aa0c0342
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2026-01-05 18:46:33 +0530

    Time: 108 ms (24.68%) | Memory: 26.8 MB (6.96%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -2,20 +2,16 @@ class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         su = 0
-        minValue = 99999999
-        flag = False
+        minValue = 999999
+    
         negCount = 0
         for i in range(n):
             for j in range(n):
                 if matrix[i][j] <0:
                     negCount += 1
-                   
-                if matrix[i][j]==0:
-                    flag = True
                 minValue = min(minValue,abs(matrix[i][j]))
                 su += abs(matrix[i][j])
-        if flag:
-            return su
+        
         if negCount%2==1:
             
             su -= 2*minValue
