4b6ee54dbd9ae8776057fa02886661b98580da56
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2026-01-10 12:40:53 +0530

    Time: 971 ms (9.75%) | Memory: 226.7 MB (5.10%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,40 +1,26 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        
-        # Make sure s2 is smaller string
-        if len(s1) < len(s2):
-            return self.minimumDeleteSum(s1 = s2, s2 = s1)
-        
-        # Case for empty s1
-        m, n = len(s1), len(s2)
-        curr_row = [0] * (n + 1)
-        for j in range(1, n + 1):
-            curr_row[j] = curr_row[j - 1] + ord(s2[j - 1])
-        
-        # Compute answer row-by-row
-        for i in range(1, m + 1):
+        n = len(s1)
+        m = len(s2)
+        @lru_cache(None)
+        def fn(i,j):
+            if i<0 and j<0:
+                return 0
+            if i<0:
+                cost = 0 
+                for k in range(j,-1,-1):
+                    cost += ord(s2[k])
+                return cost
+            if j<0:
+                cost = 0 
+                for k in range(i,-1,-1):
+                    cost += ord(s1[k])
+                return cost
+
             
-            diag = curr_row[0]
-            curr_row[0] += ord(s1[i - 1])
-
-            for j in range(1, n + 1):
-                
-                # If characters are the same, the answer is top-left-diagonal value
-                if s1[i - 1] == s2[j - 1]:
-                    answer = diag
-                
-                # Otherwise, the answer is minimum of top and left values with
-                # deleted character's ASCII value
-                else:
-                    answer = min(
-                        ord(s1[i - 1]) + curr_row[j],
-                        ord(s2[j - 1]) + curr_row[j - 1]
-                    )
-
-                # Before overwriting curr_row[j] with the answer, save it in diag
-                # for the next column
-                diag = curr_row[j]
-                curr_row[j] = answer
+            if s1[i]==s2[j]:
+                return fn(i-1,j-1)
+            else:
+                return min(min(ord(s1[i])+fn(i-1,j), ord(s2[j])+fn(i,j-1)),ord(s2[j])+ord(s1[i])+fn(i-1,j-1))
+        return fn(n-1,m-1)
         
-        # Return answer
-        return curr_row[-1]
