1eae85988fa1f846244dcbaf839db5f7ebb120af
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2026-02-28 15:32:14 +0530

    Time: 728 ms (54.09%) | Memory: 19.1 MB (91.82%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -1,19 +1,18 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        def countBit(n):
-            count = 0
-            while n:
-                count +=1
-                n = n//2
-            return count
+        
 
         MOD = 1000000007
-        ans = 1
-        for i in range(2,n+1):
-            bits = countBit(i)
-            ans = ans<<bits
+        ans = 0
+        length = 0
+        for i in range(1, n+1):
+            if i & (i-1) == 0:
+                length += 1
+        
+            ans = ans<<length
             ans += i
             ans = ans %MOD
+        
         return ans
 
 
