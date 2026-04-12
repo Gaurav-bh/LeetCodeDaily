10ca6076f77d71418e66807d24f115215424b187
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2026-04-12 11:01:38 +0530

    Time: 1004 ms (25.93%) | Memory: 26.4 MB (87.49%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -1,86 +1,57 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
        
+        
+
+
+
 
         def merge(arr, low, mid, high):
-    
-            len1 = mid - low + 1;
-            len2 = high - mid;
-        
-            first = [0]*len1;
-            second = [0]*len2;
-        
-            k = low
-            for i in range(0, len1):
-                first[i] = arr[k]
-                k+=1
-        
-            k = mid + 1;
-            for i in range(0, len2):
-                second[i] = arr[k]
-                k+=1
-        
-            # Storing the elements in the arr
-            # in sorted manner
-            i = 0
-            j = 0
-            k = low
-            while i < len1 and j < len2:
-                if first[i] <= second[j]:
-                    arr[k] = first[i]
-                    k+=1
-                    i+=1
+            temp = []  # temporary array
+            left = low  # starting index of left half of arr
+            right = mid + 1  # starting index of right half of arr
+
+            # storing elements in the temporary array in a sorted manner
+            while left <= mid and right <= high:
+                if arr[left] <= arr[right]:
+                    temp.append(arr[left])
+                    left += 1
                 else:
-                    arr[k] = second[j];
-                    k+=1
-                    j+=1
-        
-            # Storing the left out elements
-            # of first half
-            while i < len1:
-                arr[k] = first[i];
-                k+=1
-                i+=1
-        
-            # Storing the left out elements
-            # of right half
-            while j < len2:
-                arr[k] = second[j]
-                j+=1
-                k+=1
-        
-        # Function to count the reverse pairs
-        def countReversePairs(arr, low, mid, high):
+                    temp.append(arr[right])
+                    right += 1
+
+            # if elements on the left half are still left
+            while left <= mid:
+                temp.append(arr[left])
+                left += 1
+
+            # if elements on the right half are still left
+            while right <= high:
+                temp.append(arr[right])
+                right += 1
+
+            # transferring all elements from temporary to arr
+            for i in range(low, high + 1):
+                arr[i] = temp[i - low]
+
+        def countPairs(arr, low, mid, high):
+            right = mid + 1
             cnt = 0
-            j = mid + 1
-            for i in range(low, mid+1):
-                #print(i,j)
-                while j <= high and arr[i] > 2*arr[j]: 
-                    #print("j:-",j)
-                    j+=1
-                    #print("j:-",j)
-                cnt += (j - (mid + 1))
-        
+            for i in range(low, mid + 1):
+                while right <= high and arr[i] > 2 * arr[right]:
+                    right += 1
+                cnt += (right - (mid + 1))
             return cnt
-        
+
         def mergeSort(arr, low, high):
-            cnt = 0;
-            mid = low + (high - low) // 2;
+            cnt = 0
             if low >= high:
                 return cnt
-        
-            # Count the left half pairs
-            cnt += mergeSort(arr, low, mid)
-        
-            # Count the right half pairs
-            cnt += mergeSort(arr, mid + 1, high)
-        
-            # Function call to count the reverse pairs
-            cnt += countReversePairs(arr, low, mid, high)
-        
-            # Merging the first and second half
-            merge(arr, low, mid, high)
-        
+            mid = (low + high) // 2
+            cnt += mergeSort(arr, low, mid)  # left half
+            cnt += mergeSort(arr, mid + 1, high)  # right half
+            cnt += countPairs(arr, low, mid, high)  # Modification
+            merge(arr, low, mid, high)  # merging sorted halves
             return cnt
-        return mergeSort(nums,0,len(nums)-1)
-        
+
+        return mergeSort(nums,0 ,len(nums)-1)
