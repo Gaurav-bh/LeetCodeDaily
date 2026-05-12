725467bee78da0e751166911dc6c9422611b2116
Gaurav <65996694+Gaurav-bh@users.noreply.github.com>
2026-05-12 18:30:42 +0530

    Time: 6 ms (57.25%) | Memory: 20 MB (92.94%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        i=0
+        n=len(nums)
+        if n==1:
+            return 0
+        reachable=nums[0]
+        if nums[0]==0:
+            return 0
+        count=1
+        
+        while i<=reachable:
+            limit=reachable
+            if i==n-1:
+                return count
+            while i<=limit:
+                if i==n-1:
+                    return count
+                reachable=max(reachable,nums[i]+i)
+                i+=1
+            count+=1
+        
+
+        
